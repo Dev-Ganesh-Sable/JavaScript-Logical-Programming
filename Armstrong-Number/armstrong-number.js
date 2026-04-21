4de9e11adbf736b9981eab28104b8e9cdb843f77
Dev-Ganesh-Sable <dev.sableganesh0612@gmail.com>
2026-04-21 11:09:20 +0530

    more code added for armstrong number

--- Armstrong-Number/armstrong-number.js
+++ Armstrong-Number/armstrong-number.js
@@ -1,4 +1,31 @@
 
+// armstrong number => 153 => 1*1*1 + 5*5*5 + 3*3*3 
+// armtrong number => 153  => 1^3 + 5^3 + 3^3
+
+isArmstrong = (number) => {
+    let numOfDigit = number.toString().length;
+    let temp = number;
+    let sum = 0;
+
+    while (temp > 0) {
+        let remainder = temp % 10     // 3 :  get last digit of number
+        temp = Math.floor(temp / 10)  // 153/10 : 15.3 = 15 
+        sum = sum + Math.pow(remainder, numOfDigit)  // 3^3
+    }
+    console.log(`value of sum is ${sum}`);
+
+    if (number === sum) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+console.log(`isArmstrong number : ${isArmstrong(153)}`);
+
+
+
+// another example code 
 let number = 153;       // following armstrong number : 153,370,371,407
 
 function isArmstrong(num) {
@@ -20,3 +47,5 @@ if (isArmstrong(number)) {
 } else {
     console.log("Not Armstrong Number");
 }
+
+
